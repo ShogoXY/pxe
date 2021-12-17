203dbcd1f00788f2f7adc808a4cfdf42e3b784c0
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2021-12-17 10:31:26 +0100

    Update static_ip.sh

--- static_ip.sh
+++ static_ip.sh
@@ -1,16 +1,32 @@
 read -r -p "Are you sure? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$response" =~ ^([yY][eE][sS]|[yY]|[tT])$ ]]
 then
+sudo -s <<EOF
+sudo cp /etc/network/interfaces /etc/network/interfaces.bak
 
-sudo sed 's/dhcp/static/g' /etc/network/interfaces
+sudo sed -i 's/dhcp/static/g' /etc/network/interfaces
 
-sudo cat >> /etc/network/interfaces << EOF
-
+sudo cat >> /etc/network/interfaces << EOF1
 
     address 192.168.0.2
     gateway 192.168.0.0
     netmask 255.255.255.0
+EOF1
 EOF
+echo ""
+echo "pamiętaj by ustawić port nasłuchiwania w"
+echo ""
+echo "/etc/default/isc-dhcp-server"
+echo""
+
+cat /etc/network/interface
+
+sudo nano /etc/default/isc-dhcp-server
+
 else
-    echo " Pamiętaj o zmianie adresu IP oraz nasłuchiwaniu"
+    echo ""
+    echo " Pamiętaj o zmianie adresu IP oraz nasłuchiwaniu w:"
+    echo ""
+    echo "/etc/default/isc-dhcp-server"
+    echo ""
 fi
