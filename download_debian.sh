bf70507699308b4fdc5b21892c3fd5d9e6941b28
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2022-01-26 13:49:42 +0100

    Update download_debian.sh

--- download_debian.sh
+++ download_debian.sh
@@ -4,7 +4,7 @@
 lynx -dump -listonly https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/ |uniq -f 1|grep 'https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-11.2.0-amd64-.*.iso'|sed "s|https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/||g"|cut -c 7-|cat -b
 count=$(lynx -dump -listonly https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/ |uniq -f 1|grep 'https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-11.2.0-amd64-.*.iso'|sed "s|https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/||g"|cut -c 7-|wc -l)
 
-echo $count
+#echo $count
 zero=0
 echo "\n Podaj numer \n"
 while read -r iso
@@ -25,8 +25,25 @@ done
 
 
 #echo $link
-wget $link
+#wget $link
 
  
 usun=$(lynx -dump -listonly https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/ |uniq -f 1|grep 'https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-11.2.0-amd64-.*.iso'|sed "s|https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/||g"|cut -c 7-|sed -n "$iso"p)
-#rm $usun
+
+folder=$(echo $usun|rev|cut -c 5-|rev|sed "s|-live-11.2.0-amd64||g")
+
+sudo mkdir -p /srv/tftp/iso/$folder
+sudo umount /mnt
+sudo mount $usun /mnt
+sudo gcp -rf /mnt/. /srv/tftp/iso/$folder
+sudo umount /mnt
+
+echo "plik został rozpakowany do folderu /srv/tftp/iso/$folder"
+echo ""
+echo "czy chcesz usunąć plik ISO? [y/N]"
+read -r odp
+if [[ "$odp" =~ ^([yY][eE][sS]|[yY]|[tT])$ ]]; then
+sudo rm $usun
+else
+echo "plik nie został usunięty"
+fi
