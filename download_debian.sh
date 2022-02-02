6fd1f3ba217ddf61f7a807dfa0f5d371406a9282
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2022-02-02 09:52:54 +0100

    Update download_debian.sh

--- download_debian.sh
+++ download_debian.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#its simple script to download latest live debian iso
 
 lynx -dump -listonly https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/ |uniq -f 1|grep 'https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-11.2.0-amd64-.*.iso'|sed "s|https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/||g"|cut -c 7-|cat -b
 count=$(lynx -dump -listonly https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/ |uniq -f 1|grep 'https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-11.2.0-amd64-.*.iso'|sed "s|https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/||g"|cut -c 7-|wc -l)
@@ -47,3 +48,14 @@ sudo rm $usun
 else
 echo "plik nie został usunięty"
 fi
+
+echo `
+LABEL debian iso
+	MENU LABEL ^debian iso
+	KERNEL iso/$folder/live/vmlinuz-5.10.0-10-amd64
+	APPEND initrd=iso/$folder/live/initrd.img-5.10.0-10-amd64 nfsroot=192.168.0.2:/srv/tftp/iso/$folder/ ro netboot=nfs vga=0x317 boot=live ip=dhcp ---
+	TEXT HELP
+		Debian Live iso using TFTP
+	ENDTEXT
+
+`
